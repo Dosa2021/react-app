09f8646e558568aab12a28b8b981ed5692ddb26a
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-29 02:06:34 -0700

    cssでスタイリング

--- react-sample-app/src/App.jsx
+++ react-sample-app/src/App.jsx
@@ -1,5 +1,33 @@
+import './style.css'
+
 export const App = () => {
   return (
-    <h1>Hello</h1>
+    <>
+      <div className="input-area">
+        <input placeholder='TODOを入力' />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>完了です</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <li>
+            <p>完了です</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
