c4f065a82debdf0d270cf0a8ec6c3c6e740e54b8
Dosa2021 <69082197+Dosa2021@users.noreply.github.com>
2025-06-01 21:34:13 -0700

    TODOの追加機能実装 (#1)
    * useStateを使用してリスト表示
    
    * TODOの追加機能実装
    
    ---------
    
    Co-authored-by: dosaken <dosa@dosakennoMacBook-Pro.local>

--- react-sample-app/src/App.jsx
+++ react-sample-app/src/App.jsx
@@ -1,22 +1,46 @@
-import './style.css'
+import { useState } from 'react';
+import './style.css';
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('')
+  const [inCompleteTodos, setInCompleteTodos] = useState([
+    'TODO_1です',
+    'TODO_2です'
+  ])
+  const [completeTodos, setCompleteTodos] = useState([
+    'TODO_1でした',
+    'TODO_2でした'
+  ])
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === '') return
+    const newTodos = [...inCompleteTodos, todoText]
+    setInCompleteTodos(newTodos)
+    setTodoText('')
+  }
+
   return (
     <>
       <div className="input-area">
-        <input placeholder='TODOを入力' />
-        <button>追加</button>
+        <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p>完了です</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {inCompleteTodos.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </div>
       <div className="complete-area">
